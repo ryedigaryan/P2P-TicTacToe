6e69bb2c17602ed08739ce66b2c45ee28a7a41b0
ryedigaryan <ruben.yedigaryan@gmail.com>
2019-03-24 00:29:57 +0400

    ✅ AbstractAppFlowItemStateChangeListener has been added for convenience.

---
+++ generic-application/src/main/java/genericapp/AbstractAppFlowItemStateChangeListener.java
@@ -0,0 +1,23 @@
+package genericapp;
+
+public abstract class AbstractAppFlowItemStateChangeListener implements AppFlowItemStateChangeListener {
+    @Override
+    public void appFlowItemPaused() {
+
+    }
+
+    @Override
+    public void appFlowItemStarted() {
+
+    }
+
+    @Override
+    public void appFlowItemResumed() {
+
+    }
+
+    @Override
+    public void appFlowItemStopped() {
+
+    }
+}
